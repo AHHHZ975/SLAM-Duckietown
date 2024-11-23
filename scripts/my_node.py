87586689a4b26d85394bf55e8714e49fadbca541
AHHHZ975 <ahz.975@yahoo.com>
2024-11-23 08:00:09 -0500

    changes

--- scripts/my_node.py
+++ scripts/my_node.py
@@ -2,31 +2,36 @@
 
 import os
 import rospy
-from duckietown import DTROS
-from std_msgs.msg import String
-from sensor_msgs.msg import CameraInfo
-from duckietown_msgs.msg import Pose2DStamped, WheelsCmdStamped
+#from duckietown import DTROS
+#from std_msgs.msg import String
+#from sensor_msgs.msg import CameraInfo
+#from duckietown_msgs.msg import Pose2DStamped, WheelsCmdStamped
 import rosbag
 import cv2
-from cv_bridge import CvBridge
+#from cv_bridge import CvBridge
 from duckietown_utils import rgb_from_ros
 
 print("hello")
 
-bag = rosbag.Bag("/duckietown/my-ros-program/dec_17/luthor_2019-12-17-23-57-40.bag")
+import sys
 
-bridge = CvBridge()
+_, ros_bag_dir, ros_topic, output_dir = sys.argv
+print(ros_bag_dir, ros_topic, output_dir)
+
+bag = rosbag.Bag(ros_bag_dir)
+
+#bridge = CvBridge()
 count = 0
 
 time = []
 
-for topic,msg, t in bag.read_messages(topics = ["/luthor/camera_node/image/compressed"]):
+for topic,msg, t in bag.read_messages(topics = [ros_topic]):
     
     # Converting the messages related to the camera node existing in the bag file into the RGB image format that is compatible with OpenCV's format
     cv_img = rgb_from_ros(msg)
 
     # Storing the image frame somewhere in the hard disk
-    cv2.imwrite(os.path.join("/duckietown/my-ros-program/dec_17/images_luthor_2019-12-17-23-57-40", "frame%06i.png" %count), cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR))
+    cv2.imwrite(os.path.join(output_dir, "frame%06i.png" %count), cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR))
     count +=1
 
 bag.close()
