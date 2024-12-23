e5ea32e5832f9915dbc57b0fd852dbc56bbed8c8
Kevin Lessard <kevinlessard74@gmail.com>
2024-12-23 18:32:59 -0500

    useless files

--- dts-reset-exp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-dts duckiebot virtual stop vquackgpt
-dts duckiebot virtual start vquackgpt
-sleep 20
-dts code build -R vquackgpt
-docker rm dts-start-matrix
-
-echo "Virtual Duckiebot startedm, code built and dts-start-matrix docker removed."
--- run_ros_noetic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm --name ros-noetic-container -v ~/code/SLAM-Duckietown/:/workspace osrf/ros:noetic-desktop-full bash
